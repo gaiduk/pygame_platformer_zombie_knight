38edc8e9a26d7d32c814f1586f469596cd4b76df
algorithmdelta <algorithmdelta@gmail.com>
2024-04-08 18:28:44 +0300

    Player done

--- main.py
+++ main.py
@@ -31,13 +31,11 @@ class Game():
         self.title_font = pygame.font.Font("fonts/Poultrygeist.ttf", 48)
         self.HUD_font = pygame.font.Font("fonts/Pixel.ttf", 25)
 
-
     def update(self):
         self.frame_count += 1
         if self.frame_count % FPS == 0:
             self.round_time -= 1
             self.frame_count = 0
-
 
     def draw(self):
         WHITE = (255, 255, 255)
@@ -53,7 +51,7 @@ class Game():
 
         title_text = self.title_font.render("Knight vs Zombies", True, GREEN)
         title_rect = title_text.get_rect()
-        title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT - 26)
+        title_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 26)
 
         round_text = self.HUD_font.render("Round: " + str(self.round_number), True, GREEN)
         round_rect = round_text.get_rect()
@@ -117,42 +115,299 @@ class Tile(pygame.sprite.Sprite):
         self.rect.topleft = (x, y)
 
 
-
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
+    def __init__(self, x, y, platform_group, portal_group, bullet_group):
+        super().__init__()
+
+        self.HORIZONTAL_ACCELERATION = 2
+        self.VERTICAL_ACCELERATION = 0.8  # Gravity
+        self.HORIZONTAL_FRICTION = 0.15
+        self.VERTICAL_JUMP_SPEED = 18
+        self.STARTING_HEALTH = 100
+
+        # Animations frames
+        self.move_right_sprites = []
+        self.move_left_sprites = []
+
+        self.idle_right_sprites = []
+        self.idle_left_sprites = []
+
+        self.jump_right_sprites = []
+        self.jump_left_sprites = []
+
+        self.attack_right_sprites = []
+        self.attack_left_sprites = []
+
+        # Moving
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (1).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (2).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (3).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (4).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (5).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (6).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (7).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (8).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (9).png"), (64, 64)))
+        self.move_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/run/Run (10).png"), (64, 64)))
+
+        # change direction to the left side
+        for sprite in self.move_right_sprites:
+            self.move_left_sprites.append(pygame.transform.flip(sprite, True, False))
+
+        # Idle
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (1).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (2).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (3).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (4).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (5).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (6).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (7).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (8).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (9).png"), (64, 64)))
+        self.idle_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/idle/Idle (10).png"), (64, 64)))
+
+        # change direction to the left side
+        for sprite in self.idle_right_sprites:
+            self.idle_left_sprites.append(pygame.transform.flip(sprite, True, False))
+
+        # Jump
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (1).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (2).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (3).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (4).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (5).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (6).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (7).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (8).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (9).png"), (64, 64)))
+        self.jump_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/jump/Jump (10).png"), (64, 64)))
+
+        # change direction to the left side
+        for sprite in self.jump_right_sprites:
+            self.jump_left_sprites.append(pygame.transform.flip(sprite, True, False))
+
+        # Attack
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (1).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (2).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (3).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (4).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (5).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (6).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (7).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (8).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (9).png"), (64, 64)))
+        self.attack_right_sprites.append(
+            pygame.transform.scale(pygame.image.load("images/player/attack/Attack (10).png"), (64, 64)))
+
+        # change direction to the left side
+        for sprite in self.attack_right_sprites:
+            self.attack_left_sprites.append(pygame.transform.flip(sprite, True, False))
+
+        # Load img and get rect
+        self.current_sprite = 0
+        self.image = self.idle_right_sprites[self.current_sprite]
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x, y)
+
+        self.platform_group = platform_group
+        self.portal_group = portal_group
+        self.bullet_group = bullet_group
+
+        # Animation booleans
+        self.animate_jump = False
+        self.animate_fire = False
+
+        # Sounds
+        self.jump_sound = pygame.mixer.Sound("sounds/jump_sound.wav")
+        self.slash_sound = pygame.mixer.Sound("sounds/slash_sound.wav")
+        self.portal_sound = pygame.mixer.Sound("sounds/portal_sound.wav")
+        self.hit_sound = pygame.mixer.Sound("sounds/player_hit.wav")
+
+        # kinematics vectors
+        self.position = vector(x, y)
+        self.velocity = vector(0, 0)
+        self.acceleration = vector(0, self.VERTICAL_ACCELERATION)
+
+        # Set initial player variables
+        self.health = self.STARTING_HEALTH
+        self.starting_x = x
+        self.starting_y = y
 
     def update(self):
-        pass
+        self.move()
+        self.check_collisions()
+        self.check_animations()
 
     def move(self):
-        pass
+        # Set acceleration vector
+        self.acceleration = vector(0, self.VERTICAL_ACCELERATION)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.acceleration.x = -1 * self.HORIZONTAL_ACCELERATION
+            self.animate(self.move_left_sprites, 0.5)
+        elif keys[pygame.K_RIGHT]:
+            self.acceleration.x = self.HORIZONTAL_ACCELERATION
+            self.animate(self.move_right_sprites, 0.5)
+        else:
+            if self.velocity.x > 0:
+                self.animate(self.idle_right_sprites, 0.5)
+            else:
+                self.animate(self.idle_left_sprites, 0.5)
+
+        # Calculate new kinematics values
+        self.acceleration.x -= self.velocity.x * self.HORIZONTAL_FRICTION
+        self.velocity += self.acceleration
+
+        self.position += self.velocity + 0.5 * self.acceleration
+
+        # Update rect
+        if self.position.x < 0:
+            self.position.x = WINDOW_WIDTH
+        elif self.position.x > WINDOW_WIDTH:
+            self.position.x = 0
+
+        self.rect.bottomleft = self.position
 
     def check_collisions(self):
-        pass
+        if self.velocity.y > 0:
+            collided_platforms = pygame.sprite.spritecollide(self, self.platform_group, False)
+            if collided_platforms:
+                self.position.y = collided_platforms[0].rect.top + 1
+                self.velocity.y = 0
+
+        # Collision when jumping up
+        if self.velocity.y < 0:
+            collided_platforms = pygame.sprite.spritecollide(self, self.platform_group, False)
+            if collided_platforms:
+                self.velocity.y = 0
+                while pygame.sprite.spritecollide(self, self.platform_group, False):
+                    self.position.y += 1
+                    self.rect.bottomleft = self.position
+
+        # Collision for portals
+        if pygame.sprite.spritecollide(self, self.portal_group, False):
+            self.portal_sound.play()
+
+            if self.position.x > WINDOW_WIDTH // 2:
+                self.position.x = 86
+            else:
+                self.position.x = WINDOW_WIDTH - 150
+
+            if self.position.y > WINDOW_HEIGHT // 2:
+                self.position.y = 64
+            else:
+                self.position.y = WINDOW_HEIGHT - 132
+
+            self.rect.bottomleft = self.position
 
     def check_animations(self):
-        pass
+        if self.animate_jump:
+            if self.velocity.x > 0:
+                self.animate(self.jump_right_sprites, 0.1)
+            else:
+                self.animate(self.jump_left_sprites, 0.1)
+
+        if self.animate_fire:
+            if self.velocity.x > 0:
+                self.animate(self.attack_right_sprites, 0.26)
+            else:
+                self.animate(self.attack_left_sprites, 0.26)
 
     def jump(self):
-        pass
+        if pygame.sprite.spritecollide(self, self.platform_group, False):
+            self.jump_sound.play()
+            self.velocity.y = -1 * self.VERTICAL_JUMP_SPEED
+            self.animate_jump = True
 
     def fire(self):
-        pass
+        self.slash_sound.play()
+        Bullet(self.rect.centerx, self.rect.centery, self.bullet_group, self)
+        self.animate_fire = True
 
     def reset(self):
-        pass
+        self.position = vector(self.starting_x, self.starting_y)
+        self.rect.bottomleft = self.position
 
-    def animate(self):
-        pass
+    def animate(self, sprite_list, speed):
+        if self.current_sprite < len(sprite_list) - 1:
+            self.current_sprite += speed
+        else:
+            self.current_sprite = 0
+            # End jump and attack anim
+            if self.animate_jump:
+                self.animate_jump = False
+            if self.animate_fire:
+                self.animate_fire = False
+
+        self.image = sprite_list[int(self.current_sprite)]
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
+    def __init__(self, x, y, bullet_group, player):
+        super().__init__()
+
+        self.VELOCITY = 20
+        self.RANGE = 500
+
+        if player.velocity.x > 0:
+            self.image = pygame.transform.scale(pygame.image.load("images/player/slash.png"), (32, 32))
+        else:
+            self.image = pygame.transform.scale(pygame.transform.flip(pygame.image.load("images/player/slash.png"),
+                                                                      True, False), (32, 32))
+            self.VELOCITY = -1 * self.VELOCITY
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+        self.starting_x = x
+
+        bullet_group.add(self)
 
     def update(self):
-        pass
+        self.rect.x += self.VELOCITY
+
+        if abs(self.rect.x - self.starting_x) > self.RANGE:
+            self.kill()
 
 
 class Zombie(pygame.sprite.Sprite):
@@ -235,16 +490,26 @@ class Portal(pygame.sprite.Sprite):
 
         # Portal animations
         if colour == "green":
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile000.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile001.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile002.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile003.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile004.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile005.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile006.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile007.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile008.png"), (72, 72)))
-            self.portal_sprites.append(pygame.transform.scale(pygame.image.load("images/portals/green/tile009.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile000.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile001.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile002.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile003.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile004.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile005.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile006.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile007.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile008.png"), (72, 72)))
+            self.portal_sprites.append(
+                pygame.transform.scale(pygame.image.load("images/portals/green/tile009.png"), (72, 72)))
             self.portal_sprites.append(
                 pygame.transform.scale(pygame.image.load("images/portals/green/tile010.png"), (72, 72)))
             self.portal_sprites.append(
@@ -348,8 +613,8 @@ my_portal_group = pygame.sprite.Group()
 my_ruby_group = pygame.sprite.Group()
 
 # Create a tile map
-#0 -> no tile, 1 -> dirt, 2-5 -> platforms, 6 -> ruby maker, 7-8 -> portals, 9 -> player
-#23 rows and 40 columns
+# 0 -> no tile, 1 -> dirt, 2-5 -> platforms, 6 -> ruby maker, 7-8 -> portals, 9 -> player
+# 23 rows and 40 columns
 tile_map = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
      0, 0],
@@ -423,7 +688,8 @@ for i in range(len(tile_map)):
         elif tile_map[i][j] == 8:
             Portal(j * 32, i * 32, "purple", my_portal_group)
         elif tile_map[i][j] == 9:
-            pass
+            my_player = Player(j * 32 - 32, i * 32 + 32, my_platform_group, my_portal_group, my_bullet_group)
+            my_player_group.add(my_player)
 
 background_image = pygame.transform.scale(pygame.image.load("images/background.png"), (1280, 736))
 background_rect = background_image.get_rect()
@@ -438,6 +704,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                my_player.jump()
+
+            if event.key == pygame.K_SPACE:
+                my_player.fire()
 
     # blit bg
     display_surface.blit(background_image, background_rect)
@@ -449,6 +721,12 @@ while running:
     my_portal_group.update()
     my_portal_group.draw(display_surface)
 
+    my_player_group.update()
+    my_player_group.draw(display_surface)
+
+    my_bullet_group.update()
+    my_bullet_group.draw(display_surface)
+
     # Draw and update game
     my_game.update()
     my_game.draw()
@@ -456,6 +734,5 @@ while running:
     # Update display and tick clock
     pygame.display.update()
     clock.tick(FPS)
-
 
 pygame.quit()
